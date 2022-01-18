a0d8fbe138feacc11435f570fbc6e7663a91dc45
LuckyCharms1444 <jonr15563@gmail.com>
2022-01-18 10:50:30 -0500

    smiley face

---
+++ HomeWork/Composition1/js/comp.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(800,800);
+    stroke(255,255,255);
+    fill(255,90,90);
+    circle(150,150,40);
+    circle(300,150,40);
+    fill(186,255,180);
+    arc(200,300,200,200,50,PI, CHORD)
+}
+
+function draw(){
+
+}
