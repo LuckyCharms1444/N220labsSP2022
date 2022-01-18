934cd7e4a838bf60a7e090ff19607921051de2d5
LuckyCharms1444 <jonr15563@gmail.com>
2022-01-18 12:18:39 -0500

    finish

--- HomeWork/Composition1/js/comp.js
+++ HomeWork/Composition1/js/comp.js
@@ -1,8 +1,12 @@
 //JON ROSS RICHARDSON 
 //1/18/2022
 //N220
-let size = 10;
+//declare world variables
+let x = 200;
+let y = 200;
+let size = 50;
 
+//setup for the canvas and creatio
 function setup(){
     createCanvas(800,800);
     /*
@@ -15,9 +19,19 @@ function setup(){
     */
 }
 
+//function to get things on the screen 
 function draw(){
-    size = size + 1;
-    console.log(size);
+
     stroke(255,90,90);
-    circle(size,100,50);
+    fill(40,55,250);
+    circle(x,y,size + 50);
+    rect(x+100,y+100,x-100,y-100);
+    circle(x+300,y,size + 50);
+    fill(255,100,50);
+    circle(x,y,size);
+    circle(x+300,y,size);
+    fill(255,0,0);
+    arc(x+150,y+300,200,200,50,PI, CHORD)
+
+
 }
