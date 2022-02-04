4854d13c92a3f1c8999c48b6008c9596e9df1e1d
LuckyCharms1444 <jonr15563@gmail.com>
2022-02-04 13:02:06 -0500

    Objects part 1

---
+++ HomeWork/ObjectExercises/Pet/js/app.js
@@ -0,0 +1,23 @@
+/*
+Jon Ross Richardson
+2/3/22
+n220
+*/
+
+//object
+let pet = {
+    x:50,
+    y:100,
+    offset:0,
+};
+
+
+//canvas call
+function setup(){
+    createCanvas(600,800);
+}
+
+//draw call
+function draw(){
+    
+}
