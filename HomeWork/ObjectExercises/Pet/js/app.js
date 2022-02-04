86d0a57cc4f25edd9dfe35482a7262f4aaff04bb
LuckyCharms1444 <jonr15563@gmail.com>
2022-02-04 14:42:40 -0500

    pet finished, ship it

--- HomeWork/ObjectExercises/Pet/js/app.js
+++ HomeWork/ObjectExercises/Pet/js/app.js
@@ -6,8 +6,9 @@ n220
 
 //object
 let pet = {
-    x:50,
+    x:100,
     y:100,
+    size:50,
     offset:0,
 };
 
@@ -19,5 +20,22 @@ function setup(){
 
 //draw call
 function draw(){
-    
+    //refreash background
+    background(0);
+    //make x and y the mouse
+    pet.x = mouseX;
+    pet.y = mouseY;
+
+    //make offset subtract
+    if(mouseIsPressed && pet.offset > 0){
+        pet.offset = pet.offset - 1;
+        //console.log(pet.offset);
+    }
+    //make offset add 
+    if(!mouseIsPressed){
+        pet.offset = pet.offset + 1;
+        //console.log(pet.offset);
+    }
+    //draw circle
+    circle(pet.x + pet.offset,pet.y,pet.size);
 }
